5b720757caeb7f7f1980437024b2a4333957d1f9
Francisco Castañeda <dragalichi@gmail.com>
2021-12-12 08:06:01 -0600

    Use keyboard

---
+++ Keyboard.py
@@ -0,0 +1,48 @@
+import pygame
+from pygame.locals import *
+
+# Start pygame
+pygame.init()
+
+# Create a Sprite
+sprite1 = pygame.image.load("./images/humingbird.png")
+
+# Change size of sprite
+sprite1 = pygame.transform.scale(sprite1, (64, 64))
+
+# Create a screen
+screen = pygame.display.set_mode((640, 480), 0, 32)
+pygame.display.set_caption("Hello Keyboard")
+# Use different rgb to change backgroudn color
+screen.fill((126, 230, 242))
+
+game_over = False
+
+# This variable will hold the position of our sprite
+x, y = (0, 0)
+
+while not game_over:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game_over = True
+
+    # This is in charge of reading all pressed keys
+    pressed = pygame.key.get_pressed()
+
+    # This logical statement will determine what direction we will move our sprite
+    if pressed[K_UP]:
+        y -= 0.5
+    if pressed[K_DOWN]:
+        y += 0.5
+    if pressed[K_LEFT]:
+        x -= 0.5
+    if pressed[K_RIGHT]:
+        x += 0.5
+        
+    # Renders the sprite
+    screen.blit(sprite1, (x, y))
+
+    # Refresh the screen to make visible the sprite
+    pygame.display.update()
+
+pygame.quit()
--- README.md
+++ README.md
@@ -6,8 +6,8 @@ This is a simple repository where I will creating basic stuff with pygame.
 
 The tools used in this repository are
 
-* python 3.9.2
-* pygame 2.1.0
+* [python 3.9.2](https://www.python.org/)
+* [pygame 2.1.0](www.pygame.org)
 
 Also I made the repository in a raspberry pi 4.
 
@@ -34,3 +34,9 @@ In this script we show how to draw a image that is store in images called burger
 Here we show how can we resize a sprite to make it the size we want it, and keep it centered programaticly
 
 ![jochito](./images/miniJochito.png)
+
+### Keyboard.py
+
+In this cript we will make our new humingbird move around the skies, for tham matter we will teach how to use the keaybord as a controler as changing the background color of our screen.
+
+![humingbird](./images/humingbird.png)
